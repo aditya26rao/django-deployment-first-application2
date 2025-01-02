061668f038615161fa4f04d828dc6d9624abdcd5
aditya26rao <bonthapalliadityarao@gmail.com>
2025-01-02 19:44:24 +0530

    Update commit views.py

--- FirstProject/App1/views.py
+++ FirstProject/App1/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def f11(request):
     return HttpResponse("<h1>Hello User this is App1 django Webpage</h1>")
-def f22(request(:
+def f12(request(:
         return HttpResponse("<h1>Hello User </h1>")
